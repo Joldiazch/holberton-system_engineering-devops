201e22d8587c9a3516edda56fd98c86de4b503bc
joldiazch <jluis.diaz@udea.edu.co>
2020-05-04 16:27:07 +0000

    export data in the JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script that, using REST API,
+for a given employee ID, returns information
+about his/her todo list progress
+"""
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    id = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    task_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+
+    user_response = requests.get(user_url)
+    task_response = requests.get(task_url)
+
+    user_data = user_response.json()
+    task_data_list = task_response.json()
+
+    USERNAME = user_data.get('username')
+
+    tasks = {id: [
+        {
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': USERNAME
+        } for task in task_data_list
+    ]}
+
+    name_file = "{}.json".format(id)
+    with open(name_file, 'w') as f:
+        json.dump(tasks, f)
